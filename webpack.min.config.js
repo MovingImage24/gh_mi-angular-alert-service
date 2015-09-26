55cdb418b73afcfe42f72b7b4a2a0c6ad366bfc8
Enrico Hoffmann <dasrick@gmail.com>
2015-09-26 23:25:39 +0200

    play with webpack

--- webpack.min.config.js
+++ webpack.min.config.js
@@ -1,9 +1,11 @@
 var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
   plugins: [
-    new ngAnnotatePlugin({
-      add: true,
-    })
+    new ngAnnotatePlugin({add: true}),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin()
   ]
 }
