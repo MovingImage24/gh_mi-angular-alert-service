2cc499f4ad185140c885d511daf358471a3f98c9
Enrico Hoffmann <dasrick@gmail.com>
2015-12-26 09:12:00 +0100

    move pathes and filenames for distribution from package.json into webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
-var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 var webpack = require('webpack');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+var path = require('path');
+
+var srcPath = path.resolve(__dirname, 'src', 'index.js');
+var dstPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
+  entry: srcPath,
+  output: {
+    path: dstPath,
+    filename: 'mi-angular-alert-service.js'
+  },
   devtool: 'inline-source-map',
   plugins: [
     new ngAnnotatePlugin({add: true}),
-    //new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin()
   ]
